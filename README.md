72d9bb5771da17500d02766c60aca08feff4f3ab
Vladimir Shevchenko <shpymaster@gmail.com>
2021-05-06 20:18:18 +0300

    text correction

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This Python program switches On/Off Screen Saver and Screen Locker in Windows 10
 May be convenient when you use your PC in different environments (like office and home).
 
 ## Usage
-pythonw.exe Sh-ScrLock.py -on|-off|-ask \[-silent]
+pythonw.exe Sh-ScrLock.py \[-on | -off | -ask \[-silent | -autoclose \[seconds]]]
--- Sh-ScrLock.py
+++ Sh-ScrLock.py
@@ -5,7 +5,7 @@ version: 2.2.20210505
 This Python program switches On/Off Screen Saver and Screen Locker in Windows 10 via system register.
 May be convenient when you use your PC in different environments (like office and home).
 
-command line: python.exe Sh-ScrLock.py [-on | -off | -ask [-silent | -autoclose [seconds]]]
+command line: pythonw.exe Sh-ScrLock.py [-on | -off | -ask [-silent | -autoclose [seconds]]]
 ------------------------'''
 
 from winreg import *
